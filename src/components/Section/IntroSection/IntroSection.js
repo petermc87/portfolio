2833e476058910a7a5078ec2c4fe2bef447c897a
petermc87 <peter.mcgibney@gmail.com>
2023-02-08 12:18:45 -0500

    added styling and fade in animations for intro section

--- src/components/Section/IntroSection/IntroSection.js
+++ src/components/Section/IntroSection/IntroSection.js
@@ -2,7 +2,10 @@ import styles from '../IntroSection/IntroSection.module.scss'
 export default function IntroSection(){
     return(
         <div className={styles.intro}>
-            <h1>Intro Section</h1>
+           <h1>Hi, <span>I'm Peter.</span></h1>
+            {/* <br/> */}
+            <p>A <span>Full-Stack Software Engineer</span> with a keen eye for modern <span>UI design</span> and a passion for <span>backend</span> technologies.</p>
+            <br/>
         </div>
     ) 
 }
