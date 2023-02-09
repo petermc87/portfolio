be3622fd8b91bc552f78a3919a4434200b2c313f
petermc87 <peter.mcgibney@gmail.com>
2023-02-09 14:00:06 -0500

    added hamburger menu, styled navbar and logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,48 @@
 import styles from '../Header/Header.module.scss'
-
+import { useState } from 'react'
 export default function Header(){
+
+    const [openHamburger, setOpenHamburger] = useState(false)
     return (
+        <>
         <div className={styles.navigation}>
-            <h1>Headers</h1>
-            <h1>HM</h1>
+            <div className={styles.logo}>
+                <div className={styles.straight}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.line}></div>
+                <div className={styles.lineTwo}></div>
+                <div className={styles.straightEnd}></div>
+            </div>
+            {!openHamburger
+                ?
+                <div onClick={()=>{setOpenHamburger(true)}}className={styles.hMenu}>
+                    <div className={styles.lineOneHm}></div>
+                    <div className={styles.lineTwoHm}></div>
+                    <div className={styles.lineThreeHm}></div>
+                </div>                
+                :
+                <div onClick={()=>{setOpenHamburger(false)}}className={styles.hMenu}>
+                    X
+                </div>     
+            }
+
         </div>
+        {openHamburger
+            ?
+                <div className={styles.hamburgerNav}>
+                    <div className={styles.navItem}><h1>Home</h1></div>
+                    <div className={styles.navItem}><h1>Resume</h1></div>
+                    <div className={styles.navItem}><h1>About</h1></div>
+                    <div className={styles.navItem}><h1>Contact</h1></div>
+                </div>
+                    :
+                <div className={styles.hamburgerNavOut}>
+                    <div className={styles.navItem}><h1>Home</h1></div>
+                    <div className={styles.navItem}><h1>Resume</h1></div>
+                    <div className={styles.navItem}><h1>About</h1></div>
+                    <div className={styles.navItem}><h1>Contact</h1></div>
+                </div>
+        }
+        </>
     )
 }
