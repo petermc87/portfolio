cd0d7a26130842d1c45f86cc09fbdb452c0862b2
petermc87 <peter.mcgibney@gmail.com>
2023-11-09 20:12:34 -0500

    feat: add text to the bottom of the footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,35 +9,38 @@ export default function Footer () {
   }
 
   return (
-    <div className={styles.footer}>
+    <>
+      <div className={styles.footer}>
+        <a href='https://www.linkedin.com/in/petermcgibney/' target='_blank' rel='noopener noreferrer'>
+          <div className={styles.linkedin}>
+            <FontAwesomeIcon icon={faLinkedinIn} className='fa-solid fa-linkedin fa-3x' />
+          </div>
+        </a>
 
-      <a href='https://www.linkedin.com/in/petermcgibney/' target='_blank' rel='noopener noreferrer'>
-        <div className={styles.linkedin}>
-          <FontAwesomeIcon icon={faLinkedinIn} className='fa-solid fa-linkedin fa-3x' />
+        <a href='https://www.facebook.com/peter.mcgibney.3/' target='_blank' rel='noopener noreferrer'>
+          <div className={styles.facebook}>
+            <FontAwesomeIcon icon={faFacebook} className='fa-solid fa-facebook fa-3x' />
+          </div>
+        </a>
+
+        <a href='https://github.com/petermc87' target='_blank' rel='noopener noreferrer'>
+          <div className={styles.github}>
+            <FontAwesomeIcon icon={faGithub} className='fa-solid fa-github fa-3x' />
+          </div>
+        </a>
+
+        <a href='https://www.instagram.com/petermc/' target='_blank' rel='noopener noreferrer'>
+          <div className={styles.instagram}>
+            <FontAwesomeIcon icon={faInstagram} className='fa-solid fa-instagram fa-3x' />
+          </div>
+        </a>
+
+        <div className={styles.envelope} onClick={() => { sendEmail() }}>
+          <FontAwesomeIcon icon={faEnvelope} className='fa-solid fa-envelope fa-3x' />
         </div>
-      </a>
-
-      <a href='https://www.facebook.com/peter.mcgibney.3/' target='_blank' rel='noopener noreferrer'>
-        <div className={styles.facebook}>
-          <FontAwesomeIcon icon={faFacebook} className='fa-solid fa-facebook fa-3x' />
-        </div>
-      </a>
-
-      <a href='https://github.com/petermc87' target='_blank' rel='noopener noreferrer'>
-        <div className={styles.github}>
-          <FontAwesomeIcon icon={faGithub} className='fa-solid fa-github fa-3x' />
-        </div>
-      </a>
-
-      <a href='https://www.instagram.com/petermc/' target='_blank' rel='noopener noreferrer'>
-        <div className={styles.instagram}>
-          <FontAwesomeIcon icon={faInstagram} className='fa-solid fa-instagram fa-3x' />
-        </div>
-      </a>
-
-      <div className={styles.envelope} onClick={() => { sendEmail() }}>
-        <FontAwesomeIcon icon={faEnvelope} className='fa-solid fa-envelope fa-3x' />
       </div>
-    </div>
+      <div className={styles.bottomText}>Peter McGibney, 2023. All rights reserved.</div>
+    </>
+
   )
 }
